c63ae3d297ee2e40d7fc03e67487420eaa451fa4
David-DN715 <dvdjoseferreira@gmail.com>
2022-01-19 20:50:36 -0300

    primeiros passos em C# e .NET

---
+++ instalacao-e-configuracao-testes/Intrucoes.md
@@ -0,0 +1,8 @@
+
+# testes!
+
+* primeiro criamos um codigo para ser testado!
+* Logo após criamos aparte de testes com codigos também!
+
+## how to function a tests AAA?
+* [teste AAA](https://docs.microsoft.com/pt-br/visualstudio/test/unit-test-basics?view=vs-2022#:~:text=O%20padr%C3%A3o%20AAA%20%28Arrange%2C%20Act%2C%20Assert%29%20%C3%A9%20uma,que%20s%C3%A3o%20passados%20para%20o%20m%C3%A9todo%20sendo%20testado.)
---
+++ instalacao-e-configuracao-testes/contasTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using contas;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace contas.Tests
+{
+    [TestClass()]
+    public class contasTests
+    {
+        [TestMethod()]
+        public void SomaTest()
+        {
+            var a = 5;
+            var b = 5;
+            var esperado = 10;
+            var contas = new contas();
+
+            var result = contas.Soma(a, b);
+            Assert.AreEqual(esperado, result);
+        }
+
+        [TestMethod()]
+        public void subtracaoTest()
+        {
+
+            var a = 5;
+            var b = 5;
+            var esperado = 0;
+            var contas = new contas();
+
+            var result = contas.subtracao(a, b);
+            Assert.AreEqual(esperado, result);
+        }
+
+        [TestMethod()]
+        public void divisaoTest()
+        {
+
+            var a = 5;
+            var b = 5;
+            var esperado = 1;
+            var contas = new contas();
+
+            var result = contas.divisao(a, b);
+            Assert.AreEqual(esperado, result);
+        }
+
+        [TestMethod()]
+        public void multiplicacaoTest()
+        {
+
+            var a = 5;
+            var b = 5;
+            var esperado = 25;
+            var contas = new contas();
+
+            var result = contas.multiplicacao(a, b);
+            Assert.AreEqual(esperado, result);
+        }
+    }
+}
