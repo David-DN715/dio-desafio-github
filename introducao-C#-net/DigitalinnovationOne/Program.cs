eb647e68650fec5f995b6aad967e180015cf83ec
David-DN715 <dvdjoseferreira@gmail.com>
2022-01-21 23:22:25 -0300

    Adicionado o primeiro codigo em C# .net

---
+++ introducao-C#-net/DigitalinnovationOne/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DigitalinnovationOne
+{
+    class Progam    
+    {   
+        static void Main(string[] args)
+        {
+            int numerodevezes = 5;
+            for (int i=0; i < numerodevezes; i++)
+            {
+                Console.WriteLine($"Bem vindo ao curso de .NET {i}");
+            }
+        }
+    }
+}
